2b2618054c4da2305c7051b24719c202ae7b0fc0
hujiahong0313 <785074890@qq.com>
2016-10-21 02:38:00 +0800

    git init

---
+++ Hujiahong2/app/src/main/java/com/example/hujiahong/hujiahong/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.hujiahong.hujiahong;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import nf.framework.act.browser.InnerBrowserActivity;
+
+/**
+ * 主菜单
+ */
+public class MainActivity extends MyBaseActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.navigationBarLayout.setVisibility(View.VISIBLE);
+        super.top_textview.setText("医大帮Test");
+        initView();
+        Intent intent = new Intent();
+        intent.setClass(this, InnerBrowserActivity.class);
+        intent.putExtra(InnerBrowserActivity.INTENT_SOURCE, "");
+        intent.putExtra(InnerBrowserActivity.INTENT_TITLE, "");
+        intent.putExtra(InnerBrowserActivity.INTENT_URL, "http://58.57.165.71:5001/login.aspx");
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void initView() {
+        View mainView = LayoutInflater.from(this).inflate(R.layout.main_layout, super.mainlayout, false);
+        super.mainlayout.addView(mainView);
+    }
+}
